9edaaf5a00cede8d18f1718e81561a6d3646379e
Sylvester_Divine <sylvesterdivine@outlook.com>
2023-07-24 17:44:31 +0000

    file refix

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -7,13 +7,7 @@ env.hosts = ["34.224.3.244", "100.25.152.35"]
 
 
 def do_clean(number=0):
-    """Delete out-of-date archives.
-    Args:
-        number (int): The number of archives to keep.
-    If number is 0 or 1, keeps only the most recent archive. If
-    number is 2, keeps the most and second-most recent archives,
-    etc.
-    """
+    """Delete out-of-date archives """
     number = 1 if int(number) == 0 else int(number)
 
     archives = sorted(os.listdir("versions"))
