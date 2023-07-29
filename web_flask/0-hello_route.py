a887c90c7620b649b640007879b7aff384f0e0c5
Sylvester_Divine <sylvesterdivine@outlook.com>
2023-07-29 18:05:23 +0000

    flask intro fix

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
-
+"""
+This module contains a dummy flask application
+"""
 from flask import Flask
 
 app = Flask("__name__")
 @app.route('/', strict_slashes=False)
 def hello():
-    return "Hello World!"
+    """
+    This function defines what the flask app returnes
+    """
+    return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     app.run()
